35c7ec9390308d54e7452e9dcea3994c5f0a2b28
xhoi <xhozefmandreja2@gmail.com>
2025-06-16 13:30:52 +0200

    CHANGE IN MAIN

--- src/main/java/Repository/ZInventoryApi.java
+++ src/main/java/Repository/ZInventoryApi.java
@@ -2,11 +2,13 @@ package Repository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"Entity"})
 @EnableJpaRepositories(basePackages = {"Repository"})
+@ComponentScan(basePackages = {"Controller","Service"})
 public class ZInventoryApi {
 
     public static void main(String[] args) {
